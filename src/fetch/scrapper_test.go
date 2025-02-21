457a1a1c931fd8e2501d2a21028ecf5f8c951346
cloakwiss <bithim@tutamail.com>
2025-02-21 17:45:27 +0530

    Changed the struture of the project

--- src/fetch/scrapper_test.go
+++ src/fetch/scrapper_test.go
@@ -6,9 +6,6 @@ import (
 	"github.com/cloakwiss/cobweb/fetch"
 )
 
-// I have hosted website with caddy on port 8080
-// and ran test against it to see if it making map of url and its content
-
 const Target = "http://localhost:8080"
 
 func Test_Mainloop(t *testing.T) {
