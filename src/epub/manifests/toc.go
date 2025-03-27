e669de41dc172cde701bedce1967d682b2d02bc8
cloakwiss <bithim@tutamail.com>
2025-03-27 15:28:56 +0530

    Added Marshalling for Table of Contents

--- src/epub/manifests/toc.go
+++ src/epub/manifests/toc.go
@@ -5,13 +5,16 @@
 package manifests
 
 import (
+	"bufio"
+	"encoding/xml"
+	"log"
 	"strings"
 )
 
-type Dir struct {
+type Directory struct {
 	Path    string
 	Files   []File
-	SubDirs []Dir
+	SubDirs []Directory
 }
 
 type File struct {
@@ -19,7 +22,8 @@ type File struct {
 	Name string
 }
 
-func directoryTree(root string, dirlisting []string) Dir {
+// The Input should be filtered and sorted before hand
+func directoryTree(root string, dirlisting []string) Directory {
 	var (
 		subDirs map[string][]string = make(map[string][]string)
 		files   []string
@@ -44,8 +48,8 @@ func directoryTree(root string, dirlisting []string) Dir {
 	}
 
 	var (
-		processedFiles []File = make([]File, len(files))
-		processedDirs  []Dir  = make([]Dir, 0)
+		processedFiles []File      = make([]File, len(files))
+		processedDirs  []Directory = make([]Directory, 0)
 	)
 
 	for dir, sub := range subDirs {
@@ -64,13 +68,53 @@ func directoryTree(root string, dirlisting []string) Dir {
 		processedFiles = nil
 	}
 
-	return Dir{
+	return Directory{
 		Path:    root,
 		Files:   processedFiles,
 		SubDirs: processedDirs,
 	}
 }
 
-func RootDirectory(fileslist []string) Dir {
+func GenerateDirectoryTree(fileslist []string) Directory {
 	return directoryTree("", fileslist)
+}
+
+func MarshalToc(dir Directory, writeFile *bufio.Writer) {
+	marshalToc(dir, 0, writeFile)
+}
+
+func marshalToc(dir Directory, indent int, writeFile *bufio.Writer) {
+	space := "    "
+	l0 := strings.Repeat(space, indent+0)
+	l1 := strings.Repeat(space, indent+1)
+	l2 := strings.Repeat(space, indent+2)
+
+	writeFile.WriteString(l0 + "<li>")
+	defer writeFile.WriteString(l0 + "</li>\n")
+
+	writeFile.WriteString("<span>")
+	if er := xml.EscapeText(writeFile, []byte(dir.Path)); er != nil {
+		log.Fatal("Marshalling Toc failed during writing Path")
+	}
+	writeFile.WriteString("</span>\n")
+
+	writeFile.WriteString(l1 + "<ol>\n")
+	writeFile.Flush()
+
+	for _, subdir := range dir.SubDirs {
+		marshalToc(subdir, indent+1, writeFile)
+	}
+
+	for _, file := range dir.Files {
+		writeFile.WriteString(l2 + "<li><a href=\"")
+		writeFile.WriteString(file.Path + "/" + file.Name)
+		writeFile.WriteString("\">")
+		if er := xml.EscapeText(writeFile, []byte(file.Name)); er != nil {
+			log.Fatal("Marshalling Toc failed during writing Path")
+		}
+		writeFile.WriteString("</a></li>\n")
+	}
+	writeFile.WriteString(l1 + "</ol>\n")
+	writeFile.Flush()
+
 }
